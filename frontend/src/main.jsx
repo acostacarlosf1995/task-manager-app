142d976cdb244d3c440b611dd4534a626b72c641
Carlos Acosta <carlosacostaf1995@gmail.com>
2025-05-17 22:45:12 -0600

    feat: Initial frontend setup with MUI and Redux Toolkit structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {BrowserRouter} from 'react-router-dom';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import {Provider} from "react-redux";
+import {store} from './app/store';
+
+const defaultTheme = createTheme();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <ThemeProvider theme={defaultTheme}>
+                    <CssBaseline/>
+                    <App/>
+                </ThemeProvider>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+);
